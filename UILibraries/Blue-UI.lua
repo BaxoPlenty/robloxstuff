3687461fe44f2883e2768d62db2588ba5fbfbd9d
BaxoPlenty <60827021+BaxoPlenty@users.noreply.github.com>
2021-05-18 20:05:32 +0200

    Update Blue-UI.lua

--- UILibraries/Blue-UI.lua
+++ UILibraries/Blue-UI.lua
@@ -30,6 +30,7 @@ Window.__index = Window
 
 local Theme = {
   ["MainBackground"] = Color3.fromRGB(28, 28, 35),
+  ["SidebarColor"] = Color3.fromRGB(27, 26, 32),
   ["TopGradient"] = {
     ["From"] = Color3.fromRGB(14, 116, 164),
     ["To"] = Color3.fromRGB(126, 77, 234)
@@ -73,18 +74,31 @@ end
 
 --// UI Library \\--
 
--- Window.Test() // Testing
+-- Window.Destroy()
 
-function Window.Test()
-  print("WORK!")
+function Window.Destroy()
+  Window.WindowUIObject:Destroy()
+  
+  setmetatable(Window, nil)
+end
+
+-- Window.Toggle()
+
+function Window.Toggle()
+  Window.WindowUIObject.Enabled = not Window.Toggled
+  Window.Toggled = not Window.Toggled
 end
 
 -- <Window> Libary.NewWindow(<string> Name)
 
 function Library.NewWindow(Name)
+  --// Gui Creation \\--
+
   local ScreenGui = Instance.new("ScreenGui", game.CoreGui) -- Use CoreGui so it doesn't overlap with existing stuff.
   ScreenGui.ResetOnSpawn = false
   ScreenGui.Name = "BlueUI_" .. Name
+
+  --// UI Holder Creation \\--
 
   local UIHolder = Instance.new("Frame", ScreenGui)
   UIHolder.Name = "UIHolder"
@@ -92,6 +106,8 @@ function Library.NewWindow(Name)
   UIHolder.BorderSizePixel = 0
   UIHolder.Size = UDim2.new(0.415, 0, 0.475, 0)
   UIHolder.Position = UDim2.new(0, 50, 0, 50)
+
+  --// Background Creation \\--
 
   local Background = Instance.new("Frame", UIHolder)
   Background.BackgroundColor3 = Theme["MainBackground"]
@@ -102,6 +118,8 @@ function Library.NewWindow(Name)
 
   ORoundElement(Background, 3)
 
+  --// TopGradient Creation \\--
+
   local TopGradient = Instance.new("Frame", UIHolder)
   TopGradient.Size = UDim2.new(1, 0, 0.02, 0)
   TopGradient.Position = UDim2.new(0, 0, -0.01, 0)
@@ -111,7 +129,61 @@ function Library.NewWindow(Name)
   SRoundElement(TopGradient, 1)
   Gradient(TopGradient, Theme["TopGradient"].From, Theme["TopGradient"].To, 0)
 
-  return setmetatable({WindowName = Name, WindowUIObject = ScreenGui, WindowCurrentTab = nil}, Window)
+  --// TopUnround Creation \\--
+
+  local TopUnround = Instance.new("Frame", Background)
+  TopUnround.Size = UDim2.new(1, 0, 0.06, 0)
+  TopUnround.BorderSizePixel = 0
+  TopUnround.BackgroundColor3 = Theme["MainBackground"]
+  TopUnround.Name = "TopUnround"
+
+  --// Background content Creation \\--
+
+  local BackgroundContent = Instance.new("Frame", Background)
+  BackgroundContent.Name = "Content"
+  BackgroundContent.BackgroundTransparency = 1
+  BackgroundContent.BorderSizePixel = 0
+  BackgroundContent.Size = UDim2.new(1, 0, 1,0)
+
+  --// Sidebar Creation \\--
+
+  local Sidebar = Instance.new("Frame", BackgroundContent)
+  Sidebar.Name = "Sidebar"
+  Sidebar.BackgroundColor3 = Theme["SidebarColor"]
+  Sidebar.BorderSizePixel = 0
+  Sidebar.Size = UDim2.new(0.095, 0, 1,0)
+
+  ORoundElement(Sidebar, 3)
+  
+  local STabs = Instance.new("Frame", Sidebar)
+  STabs.Name = "Tabs"
+  STabs.BackgroundTransparency = 1
+  STabs.BorderSizePixel = 0
+  STabs.Size = UDim2.new(1, 0, 1,0)
+
+  local SidebarUnround1 = TabUnround:Clone()
+  SidebarUnround1.Parent = Sidebar
+  SidebarUnround1.Size = UDim2.new(-0.06, 0, 0.06, 0)
+  SidebarUnround1.Position = UDim2.new(1, 0, 0, 0)
+  SidebarUnround1.BackgroundColor3 = Theme["SidebarColor"]
+  SidebarUnround1.Name = "Unround"
+
+  local SidebarUnround2 = SidebarUnround1:Clone()
+  SidebarUnround2.Position = UDim2.new(1, 0, 1, 0)
+  SidebarUnround2.Size = UDim2.new(-0.06, 0, -0.06)
+
+  --// TabContent Creation \\--
+
+  local TabContent = Instance.new("Frame", BackgroundContent)
+  TabContent.Name = "TabContent"
+  TabContent.BackgroundTransparency = 1
+  TabContent.BorderSizePixel = 0
+  TabContent.Size = UDim2.new(0.905, 0, 1,0)
+  TabContent.Position = UDim2.new(0.095, 0, 0, 0)
+
+  --// Return the window \\--
+
+  return setmetatable( {WindowName = Name, WindowUIObject = ScreenGui, WindowCurrentTab = nil, Toggled = true }, Window)
 end
 
 --// Finish up \\--
